90bd7e646313a0943315a742cc2a7c7feb4a1c16
fil-technology <opensource@fil.technology>
2026-04-04 11:13:41 +0300

    Fix esh 0.1.8 cask checksum

--- Casks/esh.rb
+++ Casks/esh.rb
@@ -1,6 +1,6 @@
 cask "esh" do
   version "0.1.8"
-  sha256 ""
+  sha256 "043071021d8218ae359e010f8386760ade2033c374c84e40e1ea218d3eff7d4b"
 
   url "https://github.com/fil-technology/esh/releases/download/v#{version}/esh-macos-#{version}.tar.gz"
   name "Esh"
